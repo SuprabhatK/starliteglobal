4c1c9a8bead73dba169a321a380136b992d0369b
suprabhatk17 <suprabhat.karmakar17@gmail.com>
2025-04-08 10:27:05 +0530

    change email address

--- client/src/components/common/footer/Footer.jsx
+++ client/src/components/common/footer/Footer.jsx
@@ -62,7 +62,7 @@ export default function Footer() {
                         <FaFax className="icon" /> Fax: 30113232
                     </p>                    <p>
                         <FaEnvelope className="icon" />
-                        <a href="mailto:info@starliteglobal.com">info@starliteglobal.com</a>
+                        <a href="mailto:info.starliteglobal@gmail.com">info.starliteglobal@gmail.com</a>
                     </p>
                     <p>
                         <FaClock className="icon" /> Timing 9:30 AM to 5:30 PM
